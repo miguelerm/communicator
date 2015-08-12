00c540ebd6f7cc8fcfe91cd5252312b349f17388
Miguel Román <miguelerm@gmail.com>
2015-08-12 09:43:11 -0600

    Esquema inicial (autenticación Windows)

---
+++ Communicator/Config/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Communicator.Config
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // TODO: Add any additional configuration code.
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Filters.Add(new AuthorizeAttribute());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Communicator/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Communicator.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+
+        [Route("me")]
+        public IHttpActionResult GetCurrentUser()
+        {
+            return Ok(new { Username = User.Identity.Name });
+        }
+
+    }
+}
