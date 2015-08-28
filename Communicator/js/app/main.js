773645e21eee0e6b7ac9c970ab29055f379670ff
Miguel Román <miguelerm@gmail.com>
2015-08-28 17:39:39 -0600

    mas cambios en el layout

--- Communicator/js/app/main.js
+++ Communicator/js/app/main.js
@@ -1,9 +1,32 @@
 ﻿(function () {
 
-    angular.module('communicator', ['ngMaterial']).config(function ($mdThemingProvider) {
-       /* $mdThemingProvider.theme('default')
-          .primaryPalette('grey')
-          .accentPalette('indigo');*/
-    });;
+    angular.module('communicator', ['ngMaterial']).controller('DemoController', DemoController);
+
+
+    function DemoController($scope, $mdSidenav) {
+
+        $scope.foto = 'https://material.angularjs.org/latest/img/list/60.jpeg?0';
+        $scope.nombre = 'Me';
+        $scope.conversaciones = [];
+        $scope.toggleConversaciones = toggleConversaciones;
+
+        init();
+
+        function init() {
+            for (var i = 0; i < 100; i++) {
+                $scope.conversaciones.push({
+                    imagen: 'https://material.angularjs.org/latest/img/list/60.jpeg?' + i,
+                    nombre: 'Fulanito de tal',
+                    grupo: 'desarrollo',
+                    texto: 'asdfasdf....'
+                });
+            }
+        }
+
+        function toggleConversaciones() {
+            $mdSidenav('conversaciones').toggle();
+        }
+
+    }
 
 })();
