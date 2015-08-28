8f39e2dc50b90d20426fe9a9cd20b3dcf8416477
Miguel Román <miguelerm@gmail.com>
2015-08-28 16:00:50 -0600

    layout base

---
+++ Communicator/js/app/main.js
@@ -0,0 +1,9 @@
+﻿(function () {
+
+    angular.module('communicator', ['ngMaterial']).config(function ($mdThemingProvider) {
+       /* $mdThemingProvider.theme('default')
+          .primaryPalette('grey')
+          .accentPalette('indigo');*/
+    });;
+
+})();
